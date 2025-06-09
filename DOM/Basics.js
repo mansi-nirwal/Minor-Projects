34731b252113a182a2c8db8a0c9453557fb88606
Mansi Nirwal <manshinirwal19@gmail.com>
2025-06-09 20:16:34 +0530

    define dom

---
+++ DOM/Basics.js
@@ -0,0 +1,3 @@
+/* The Document Object Model (DOM) is a programming interface that allows you to manipulate 
+the structure, style, and content of web documents dynamically.
+  If you're looking for a solid tutorial, here are some great resources:*/
