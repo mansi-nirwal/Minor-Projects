ae26fbb82879dfca7c1d758d1ebb4c37adf91c0d
mansi-nirwal <manshinirwal1989@gmail.com>
2025-11-20 12:18:16 +0530

    1

---
+++ JS-Essential/script.js
@@ -0,0 +1,40 @@
+console.log("Hi");
+
+// Const : Constant Not Change 
+
+// Let : Can Change
+
+// Var: Use before the initialise 
+
+// Sum of two integer
+
+let num = 10;
+
+let num2 = 20;
+
+console.log(num+num2);
+
+// String + String = String (Concatenation)
+
+// String + int = String( Contenation )
+
+// int + int = int ( arithmetic )
+
+
+let s1 = "Mansi";
+
+let s2 = "Nirwal";
+
+console.log(s1 + s2);
+
+// Type Coercion
+
+console.log('1' - 1); // 0
+
+console.log('1' + 1); // 11
+
+let prompt = require('prompt-sync')()
+
+let age = prompt("age");
+
+console.log(age)
